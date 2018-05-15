03155ebc4bd6c603c701cc8638c9c95e3418358c
Jesse Anderson <jesse@bigdatainstitute.io>
2018-05-15 08:09:15 -0700

    Started piping through cat so you do not have to press q each time

--- scripts/status_kafka.sh
+++ scripts/status_kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo service zookeeper status
-sudo service kafka-server status
-sudo service schema-registry status
-sudo service kafka-rest status
+sudo service zookeeper status | cat
+sudo service kafka-server status | cat
+sudo service schema-registry status | cat
+sudo service kafka-rest status | cat
